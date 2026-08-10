817c2e9c5ab06e620bd22f7be38a0e883427f0c3
zh-harry <kingkie729@gmail.com>
2026-08-10 10:04:39 +0800

    specify the range of the number in the grid

--- Introductory_Problem/Mex_Grid_Construction/main.cpp
+++ Introductory_Problem/Mex_Grid_Construction/main.cpp
@@ -3,8 +3,6 @@
 #include <cmath>
 using namespace std;
 
-constexpr int MX = 200;
-
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);
@@ -12,11 +10,11 @@ int main(){
     int n;
     cin >> n;
     vector<vector<int>> grid(n,vector<int>(n,0));
-    vector<vector<bool>> used_row(n,vector<bool>(MX,false));
-    vector<vector<bool>> used_col(n,vector<bool>(MX,false));
+    vector<vector<bool>> used_row(n,vector<bool>(2*n,false));
+    vector<vector<bool>> used_col(n,vector<bool>(2*n,false));
     for(int i = 0;i < n;++i){
         for(int j = 0;j < n;++j){
-            for(int k = 0;k < MX;++k){
+            for(int k = 0;k < 2*n;++k){
                 if((!used_row[i][k]) && (!used_col[j][k])){
                     used_row[i][k] = used_col[j][k] = true;
                     grid[i][j] = k;
